36f52786a53a4553019dbe02588da35c0b5d58ba
Jorge Queiroz <jorge.queiroz@outlook.com.br>
2021-11-28 16:41:56 -0300

    Inserção de data na persistência

--- src/main/java/br/com/jorge/calculadora/GuardaResultadoDaOperacao.java
+++ src/main/java/br/com/jorge/calculadora/GuardaResultadoDaOperacao.java
@@ -1,15 +1,22 @@
 package br.com.jorge.calculadora;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class GuardaResultadoDaOperacao {
 
 	public static void escrever(BigDecimal valor1, BigDecimal valor2, String operacao) throws IOException {
 		FileWriter fw = new FileWriter("numeroscalculados.txt", true);
-		fw.append(operacao + " de " + valor1 + " e " + valor2 + "\n");
+
+		fw.append("Dia: " + getDataAtual() + ". " + operacao + " de " + valor1 + " e " + valor2 + "." + "\n");
 		fw.close();
+	}
+
+	public static String getDataAtual() {
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return LocalDate.now().format(formatador);
 	}
 }
